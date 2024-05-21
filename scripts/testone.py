b53e5df85db5d16e95798a38b042517f5e756ef7
Fabian Wachsmann <k204210@cmip-dps.cloud.dkrz.de>
2024-05-21 15:17:11 +0200

    Try this one

--- scripts/testone.py
+++ scripts/testone.py
@@ -12,6 +12,8 @@ import nest_asyncio
 nest_asyncio.apply()
 from intake.config import conf
 conf['cache_disabled'] = True
+import threading
+import time
 
 def compress_data(partds):
     import numcodecs
@@ -28,7 +30,9 @@ if __name__ == "__main__":  # This avoids infinite subprocess creation
     os.environ["ZARR_ADDRESS"]=zarrcluster.scheduler._address
     dsdict={}
     cat=intake.open_catalog("https://gitlab.dkrz.de/data-infrastructure-services/era5-kerchunks/-/raw/main/main.yaml")
-    dsdict["pressure-level_analysis_daily"]=cat["pressure-level_analysis_daily"].to_dask()
+    ds=cat["pressure-level_analysis_daily"].to_dask()
+    ds=ds.apply_ufunc(compress_data)
+    dsdict["pressure-level_analysis_daily"]=ds
     #collection = xp.Rest([], cache_kws=dict(available_bytes=0))
     #collection.register_plugin(DynamicKerchunk())
     collection = xp.Rest(dsdict,cache_kws=dict(available_bytes=1000000000))    
@@ -37,4 +41,24 @@ if __name__ == "__main__":  # This avoids infinite subprocess creation
     collection.register_plugin(FileServe())
     collection.register_plugin(PlotPlugin())
 
-    collection.serve(host="0.0.0.0", port=9000)
+    stop_thread = False
+
+    def blocking_function():
+        while not stop_thread:
+            collection.serve(host="0.0.0.0", port=9000)
+
+# Create a thread for the blocking function
+    thread = threading.Thread(target=blocking_function)
+
+# Start the thread
+    thread.start()
+
+# Block the main thread for 3 seconds
+    time.sleep(3)
+
+# Set the flag to stop the thread
+    stop_thread = True
+
+# Optionally join the thread if you need to wait for it to finish
+    thread.join()
+
