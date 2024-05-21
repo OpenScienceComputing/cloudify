519df028eac6dabfba1f3ce6cd6bd9e39ae7c54e
Fabian Wachsmann <k204210@cmip-dps.cloud.dkrz.de>
2024-05-21 16:03:31 +0200

    restructured2

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 conda install -c conda-forge micromamba
-micromamba create -c conda-forge -n cloudify pip python==3.11 eccodes scipy -y
+micromamba env create -f environment.yaml -y 
 source activate /root/micromamba/envs/cloudify
 pip install -U .
-mkdir logs
-python scripts/testone.py > logs/log
---
+++ test/test.sh
@@ -0,0 +1,3 @@
+source activate /root/micromamba/envs/cloudify
+mkdir logs
+python testone.py > logs/log
