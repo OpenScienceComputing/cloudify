1137d4a5ee247ddb2428c615cf5ac704977e5b55
Fabian Wachsmann <k204210@cmip-dps.cloud.dkrz.de>
2024-05-21 15:19:49 +0200

    Try this

--- test/ci.sh
+++ test/ci.sh
@@ -1,4 +1,5 @@
-conda install -c conda-forge pip python==3.11 -y
-pip install -U .
+conda install -c conda-forge micromamba pip python==3.11 -y
+micromamba env create -f environment.yaml
+source activate cloudify
 mkdir logs
 python scripts/testone.py > logs/log
