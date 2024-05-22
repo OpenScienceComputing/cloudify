d4afe2fceddcece00b63ebdd9f3a87a5c34bbf7c
Fabian Wachsmann <k204210@cmip-dps.cloud.dkrz.de>
2024-05-22 09:05:10 +0200

    Ran black

--- cloudify/utils/daskhelper.py
+++ cloudify/utils/daskhelper.py
@@ -1,19 +1,22 @@
 async def get_dask_client():
     from dask.distributed import Client
+
     return await Client(
-            asynchronous=True,
-            processes=True,
-            n_workers=2,
-            threads_per_worker=4,
-            memory_limit="4GB",
-            set_as_default=False
-            )
+        asynchronous=True,
+        processes=True,
+        n_workers=2,
+        threads_per_worker=4,
+        memory_limit="4GB",
+        set_as_default=False,
+    )
+
 
 async def get_dask_cluster():
     from dask.distributed import LocalCluster
+
     return LocalCluster(
-            processes=True,
-            n_workers=2,
-            threads_per_worker=8,
-            memory_limit="16GB",
-            )
+        processes=True,
+        n_workers=2,
+        threads_per_worker=8,
+        memory_limit="16GB",
+    )
