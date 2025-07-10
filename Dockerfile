e3fd855b13fda4891b1d821744ef44fa272e2460
wachsylon <fabian.wachsmann90@gmx.de>
2025-07-10 15:47:24 +0200

    Added Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM mambaorg/micromamba:latest
+
+# Set working directory
+WORKDIR /app
+
+# Copy the installation script and environment file
+COPY --chmod=765 install.sh environment.yaml /app/
+# Copy patches directory
+COPY --chmod=765 patches /app/patches
+
+# Run the installation script
+RUN cd /app && ./install.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-conda install -c conda-forge micromamba
+#conda install -c conda-forge micromamba
 micromamba env create -f environment.yaml -y 
-source activate /root/micromamba/envs/cloudify
-pip install -U .
+eval "$(micromamba shell hook --shell bash)"
+micromamba activate cloudify
+pip install -U git+https://gitlab.dkrz.de/data-infrastructure-services/cloudify.git
 SITE_PACKAGES=$(python -c "import site; print(site.getsitepackages()[0])")
 cp patches/utilszarr.py $SITE_PACKAGES/xpublish/utils/zarr.py
 cp patches/pluginszarr.py $SITE_PACKAGES/xpublish/plugins/included/zarr.py
